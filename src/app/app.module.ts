c81f073b2db00dd748d63c92afaaa21157129c24
Futó Attila <futo.attila02@gmail.com>
2024-04-02 18:14:12 +0200

    Create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { BannerComponent } from './common/banner/banner.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BannerComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
