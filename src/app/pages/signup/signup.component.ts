c7921845eb356c48d2fcb82836bd4eaea328b2a4
Futó Attila <futo.attila02@gmail.com>
2024-04-12 16:39:49 +0200

    Add validation for registration form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 
 @Component({
@@ -7,26 +7,30 @@ import { Location } from '@angular/common';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
   
-
   signUpForm = new FormGroup({
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    passwordRepeat: new FormControl(''),
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]),
+    passwordRepeat: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]),
     name: new FormGroup({
-      firstName: new FormControl(''),
-      lastName: new FormControl('')
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required])
     }),
   });
 
-  constructor(private location: Location) {
+  constructor(private location: Location, private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
   }
 
   onSubmit(){
-    
+    if (this.signUpForm.valid) {
+      console.log('valid form');
+    } else {
+      console.log('invalid form :(');
+    }
   }
 
   goBack(){
